b50192c2b8a8251dc38aa7697e054c0e4255735a
Polkovnik-Daniil <0205danik@mail.ru>
2020-11-25 11:33:14 +0300

    final version

---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Addition
+    {
+        public string Name;
+        public string Surname;
+        public string Name_ { 
+            get 
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            } 
+        }
+        public string Surname_
+        {   
+            get
+            {
+                return Surname;
+            }
+            set
+            {
+                Surname = value;
+            }
+        }
+    }
+    public class Airline
+    {
+        public string Destination;
+        public string Type_Plane;
+        public string flight_number;
+        public string minute;
+        public string hour;
+        public string seconds;
+        public string day;
+        public string month;
+        public string years;
+        public string weekDay;
+        public int[] Day_month = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        DateTime dt1 = new DateTime();
+        public string Destination_
+        {
+            get
+            {
+                return Destination;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+                else
+                    Destination = value;
+            }
+        }
+        ///////////////////////////
+        public string Type_Plane_
+        {
+            get
+            {
+                return Type_Plane;
+            }
+            set
+            {
+                if (value == "" && fail == 0)
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+                else
+                    Type_Plane = value;
+            }
+        }
+        ///////////////////////////
+        public string flight_number_
+        {
+            get
+            {
+                return flight_number;
+            }
+            set
+            {
+                if (int.Parse(value) >= Int32.MaxValue)
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+                else
+                    flight_number = value;
+            }
+        }
+        ///////////////////////////
+        public string minute_
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (60 > Convert.ToInt32(value) && fail == 0)
+                {
+                    minute = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        ///////////////////////////
+        public string hour_
+        {
+            get
+            {
+                return hour;
+            }
+            set
+            {
+                if (24 > Convert.ToInt32(value))
+                {
+                    hour = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        public string day_
+        {
+            get
+            {
+                return day;
+            }
+            set
+            {
+                int month = int.Parse(this.month);
+                if (int.Parse(value) < 32 && Day_month[month - 1] > int.Parse(value))
+                {
+                    day = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        ///////////////////////////
+        public string month_
+        {
+            get
+            {
+                return month;
+            }
+            set
+            {
+                if (int.Parse(value) < 13)
+                {
+                    month = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        ///////////////////////////
+        public string years_
+        {
+            get
+            {
+                return years;
+            }
+            set
+            {
+                int year = Convert.ToInt32(time_Now.ToString("yyyy"));
+                if (year > Convert.ToInt32(value))
+                {
+                    years = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        // not fact
+        ///////////////////////////
+        public string[] weekDays = new string[] { "Mon", "Tues", "Wed", "Thut", "Frid", "Sat", "Sund" };
+        ///////////////////////////
+        public string weekDay_
+        {
+            get
+            {
+                return weekDay;
+            }
+            set
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    if (value != weekDays[i] && fail == 0)
+                    {
+                        Console.WriteLine("Error cant be written");
+                        fail++;
+                    }
+                    else
+                    {
+                        weekDay = value;
+                        break;
+                    }
+                }
+            }
+        }
+        ///////////////////////////
+        public int fail = 0;
+        public DateTime time_Now = DateTime.Now;
+        ///////////////////////////
+        public string seconds_
+        {
+            get
+            {
+                return seconds;
+            }
+            set
+            {
+                if (int.Parse(value) < 61)
+                {
+                    seconds = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        ///////////////////////////
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static void add(List<Airline> newlist, string Destination, string flight_number , string Type_Plane, int i, Airline air)
+        {
+            air.Destination_ = Destination;
+            air.flight_number_ = flight_number;
+            air.Type_Plane_ = Type_Plane;
+            newlist.Add(air);
+        }
+        private static void Show(List<Airline> newlist, int i)
+        {
+            Console.WriteLine("Destination:\t" + newlist[i].Destination);
+            Console.WriteLine("flight number:\t" + newlist[i].flight_number);
+            Console.WriteLine("Type Plane:\t" + newlist[i].Type_Plane);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            
+            int value = rnd.Next();
+            string[] month = { "January", "February", "March", "April", "May", "June", "July" , "August", "September", "October" , "November" , "December" };
+            string[] array = { "December", "January", "February", "June", "July", "August" };
+            int[] numbers1 = { -3, -2, -1, 0, 1, 2, 3 };
+            var result_ = numbers1.Skip(4).Take(3);
+            foreach (int i in result_)
+                Console.Write(i + ", ");
+            Console.WriteLine("\n--------------------------------------------------------------------------");
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int size = (from i in numbers where i % 2 == 0 && i > 5 select i).Count();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine(size);
+            int size_ = numbers.Max();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine(size_);
+            int size_1 = numbers.Min();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine(size_1);
+            int size_2 = (int)numbers.Average();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine(size_2);
+            var numbers_ = numbers.Skip(4);
+            foreach (var i in numbers_)
+                Console.Write(i + ", ");
+            Console.WriteLine("\n--------------------------------------------------------------------------");
+            var sequence = month.Where(p => p.Length == 6);     //amount value
+            var result = month.Intersect<string>(array);
+            var OrederBty = from t in month
+                            orderby t
+                            select t;
+            var u = from t in month
+                    where t.Contains("u") & t.Length > 4
+                    select t;
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("\n--------------------------------------------------------------------------");
+            Console.WriteLine($"Length n: 6");
+            foreach (var i in sequence)
+                Console.WriteLine(i);
+            Console.WriteLine("--------------------------------------------------------------------------\nAfter sort");
+            month.OrderBy(x => x);
+            foreach (var i in month)
+                Console.Write(i + ", ");
+
+            Console.WriteLine("\n--------------------------------------------------------------------------");
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            List<Airline> newlist = new List<Airline>();
+            string[] arrdest = { "Minsk", "Moskow", "New York", "Dybai", "Warshawa", "Berlin", "London", "Paris" };
+            for (int i = 0; i < 8; i++)
+            {
+                Airline air = new Airline();
+                string val = rnd.Next().ToString();
+                add(newlist, arrdest[i], val, "Boeing", i , air);
+            }
+            for (int i = 0; i < 8; i++)
+            {
+               Console.WriteLine("--------------------------------------------------------------------------");
+               Show(newlist, i);
+               Console.WriteLine("--------------------------------------------------------------------------");
+            }
+            IEnumerable<Airline> quest3_1 = newlist.Where(s => s.Destination == "Minsk");
+            IEnumerable<Airline> quest3_2 = newlist.Where(t => t.Type_Plane == "Boeing");
+            IEnumerable<Airline> quest3_3 = newlist.Where(t => int.Parse(t.flight_number) >= 1000000000);
+            quest3_1 = from t in newlist
+                       where t.Destination == "Minsk" & int.Parse(t.flight_number) >= 10000
+                       select t;
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------------------------\nTask 1:");
+            Console.WriteLine("Destinition|\tFlight number|\tType Plane|");
+            foreach (var i in quest3_1)
+                Console.Write(i.Destination + ",  \t" + i.flight_number + ",  \t" + i.Type_Plane + "\n");
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------------------------\nTask 2:");
+            Console.WriteLine("Destinition|\tFlight number|\tType Plane|");
+            foreach (var i in quest3_2)
+                Console.Write(i.Destination + ",  \t" + i.flight_number + ",  \t" + i.Type_Plane + "\n");
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------------------------\nTask 3:");
+            Console.WriteLine("Destinition|\tFlight number|\tType Plane|");
+            foreach (var i in quest3_3)
+                Console.Write(i.Destination + ",  \t" + i.flight_number + ",  \t" + i.Type_Plane + "\n");
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            List<Addition> list = new List<Addition>()
+            {
+                new Addition{ Name = "Vladislav" , Surname = "kurdinov"},
+                new Addition{ Name = "Vasiliy" , Surname = "Kormon"},
+                new Addition{ Name = "Solomon" , Surname = "Warpov"},
+                new Addition{ Name = "Mihael" , Surname = "Krikylin"},
+                new Addition{ Name = "Artem" , Surname = "Novokov"},
+                new Addition{ Name = "Dmitry" , Surname = "Shimonikov"},
+                new Addition{ Name = "Lisa" , Surname = "Kuchukina"},
+                new Addition{ Name = "Anton" , Surname = "Kilimov"}
+            };
+            var Result_ = newlist.Join(list,
+                    s => s.Destination_,
+                    p => p.Name_,
+                    (s, p) => new { s.Destination, s.flight_number, s.Type_Plane, p.Name, p.Surname });
+            Console.WriteLine("--------------------------------------------------------------------------\nTask 5:");
+            Console.WriteLine("Destinition|\tName|\tSurname|");
+            foreach (var i in Result_)
+                Console.Write(i.Destination + ",  \t" + ",  \t" + i.Name + ",  \t" + i.Surname + "\n");
+            Console.WriteLine("--------------------------------------------------------------------------");
+        }
+    }
+}
